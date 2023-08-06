fec359fbe05c3867f2787f25eec889bb25b4ea5f
Chris Mountford <chris.mountford.92@googlemail.com>
2023-08-06 15:57:38 +0100

    Adding extension

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -3,18 +3,20 @@ import { View, Button, Text } from 'react-native'
 
 const Timer = () => {
     const [display, setDisplay] = useState("00:45")
-    const [totalTime] = useState(45)
-    const [secsFromInitialStart, setSecsFromInitialStart] = useState(0)
+    const [totalTime, setTotalTime] = useState(45)
+    let [secsFromInitialStart, setSecsFromInitialStart] = useState(0)
     const [clock, setClock] = useState<any | null>()
     const [isPaused, setIsPaused] = useState(false)
+    const [redExt, setRedExt] = useState(false)
+    const [yellowExt, setYellowExt] = useState(false)
 
     useEffect(() => {
         if(Number(secsFromInitialStart) === Number(totalTime)) {
-            pauseClockFn()
+            stopClockFn()
         }
     }, [secsFromInitialStart])
 
-    const startTimerFn = () => {
+    const startClockFn = () => {
         const start = new Date()
 
         let secsFromLastPaused = 0
@@ -25,25 +27,50 @@ const Timer = () => {
         setClock(setInterval(() => {
             let current
             current = Number(((new Date() - start) / 1000).toFixed())
-
             current += secsFromLastPaused
 
             setSecsFromInitialStart(current)
             current = totalTime - current
+
+            if(redExt) {
+                current += 15
+            }
+
             let secs = (current % 60).toString().padStart(2, "0")
             setDisplay(`00:${secs}`)
         }, 1000))
     }
+
     const pauseClockFn = () => {
         setIsPaused(true)
         clearInterval(clock)
     }
 
+    const stopClockFn = () => {
+        clearInterval(clock)
+    }
+
+    const extensionCalled = (colour: string) => {
+        if(colour === 'red' && !redExt) {
+            console.log('Red ext called')
+            setRedExt(true)
+        }
+        else if(colour === 'yellow' && !yellowExt){ 
+            console.log('Yellow ext called')
+            setYellowExt(true)
+        }
+
+        clearInterval(clock)
+        startClockFn()
+    }
+
     return (
         <View>
             <Text>{display}</Text>
-            <Button onPress={startTimerFn} title="Start" />
+            <Button onPress={() => {startClockFn()}} title="Start" />
             <Button onPress={pauseClockFn} title="Pause" />
+            <Button onPress={() => extensionCalled('red')} title="Red Ext" />
+            <Button onPress={() => extensionCalled('yellow')} title="Yellow Ext" />
         </View>
     )
 }
